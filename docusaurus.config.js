3971d7355abb3312821173bd18706543960c75c0
rjohnsen <rogercbjohnsen@gmail.com>
2022-11-27 15:23:44 +0100

    Changed config for docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Kringlecon 2022',
+  tagline: 'Writeup Roger C.B. Johnsen',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rjohnsen', // Usually your GitHub org/user name.
+  projectName: 'kringlecon-2022', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -57,24 +57,24 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Kringlecon 2022',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'kringlecon-2022 logo',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'objectives/intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Objectives',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+            type: 'doc',
+            docId: 'terminals/intro',
+            position: 'left',
+            label: 'Terminals',
+          }
         ],
       },
       footer: {
