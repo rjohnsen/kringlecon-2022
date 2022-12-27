af146c10b95abe4220618c2626d7b69e70f59ee5
rjohnsen <rogercbjohnsen@gmail.com>
2022-12-27 13:41:13 +0100

    Flicking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,6 +73,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
     }),
 };
 
